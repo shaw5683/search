bdc5ee1129db82df2157c06d6cceaac6f44ca30f
xiaojinjin <xiaojinjin@huishoubao.com.cn>
2018-11-22 15:56:17 +0800

    no message

--- controllers/search.js
+++ controllers/search.js
@@ -3,12 +3,22 @@ const cheerio = require('cheerio');
 
 const getContent = function (data) {
 	const $ = cheerio.load(data);
-	return $('#ires ol').html();
+	const result = $('#ires ol').html();
+	let total = 0;
+	if (result === null) {
+		return 'No Result';
+	} else {
+		total = $('#resultStats').replace(/^Page\s\S+\sof\sabout\s(\S+)\sresults$/gi, '$1').replace(/,/g, '');
+	}
+	return {
+		result,
+		total
+	};
 };
 
 const getSearchRes = function (ctx) {
 	return new Promise((res, rej) => {
-		https.get(`https://www.google.com/search?q=${encodeURIComponent(ctx.query.str)}&start=${ctx.query.pageIndex || 1}0`, (resp) => {
+		https.get(`https://www.google.com/search?q=${encodeURIComponent(ctx.query.str)}&start=${(ctx.query.pageIndex - 1) || ''}0`, (resp) => {
 			let data = '';
 
 			// A chunk of data has been recieved.
@@ -18,7 +28,7 @@ const getSearchRes = function (ctx) {
 
 			// The whole response has been received. Print out the result.
 			resp.on('end', () => {
-				res(data);
+				res(getContent(data));
 			});
 
 		}).on("error", (err) => {
@@ -32,8 +42,10 @@ const search = async (ctx, next) => {
 	console.log(ctx.query);
 	try {
 		ctx.body = await getSearchRes(ctx);
+		ctx.response.type = 'json';
 	} catch (e) {
 		console.log(e);
+		ctx.response.type = 'text';
 		ctx.body = 'something wrong';
 	}
 };
